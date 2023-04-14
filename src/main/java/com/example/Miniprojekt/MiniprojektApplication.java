f6772826c6227c0f250f0a0e3275f87d6e9c3291
AmingYee <wenm0007@stud.kea.dk>
2023-04-14 19:25:52 +0200

    test

--- src/main/java/com/example/Miniprojekt/MiniprojektApplication.java
+++ src/main/java/com/example/Miniprojekt/MiniprojektApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniprojektApplication {
 
 	public static void main(String[] args) {
-		private String;
-
 		SpringApplication.run(MiniprojektApplication.class, args);
 	}
 
